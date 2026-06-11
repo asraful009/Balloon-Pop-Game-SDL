20c8232312d2ee3d5483febfaabc0a07655d349a
Asraful Islam <pavel@irinfosys.com>
2026-06-11 18:32:02 +0600

    fix image load

--- public/Balloon.h
+++ public/Balloon.h
@@ -11,33 +11,36 @@ struct Pos
     float y;
 };
 
-class Balloon 
+class Balloon
 {
 private:
     Pos m_pos;
-    SDL_Texture* m_image;
-    Window* window;
+    SDL_Texture *m_image;
+    Window *window;
 
 public:
-    Balloon(Window* window);
-    Balloon(float x, float y, const std::string& image);
+    Balloon(Window *window);
+    Balloon(float x, float y, const std::string &image);
 
     // Getters
-    Pos getXY()            const { return m_pos; };
-    float       getX()     const { return m_pos.x; }
-    float       getY()     const { return m_pos.y; }
-    SDL_Texture* getImage() const { return m_image; }
-    
+    Pos getXY() const { return m_pos; };
+    float getX() const { return m_pos.x; }
+    float getY() const { return m_pos.y; }
+    SDL_Texture *getImage() const { return m_image; }
 
     // Setters
-    void setXY(float x, float y) { m_pos.x = x; m_pos.y = y; }
-    void setX(float x)           { m_pos.x = x; }
-    void setY(float y)           { m_pos.y = y; }
-    void setImage(const std::string& image) { m_image = image; }
-    
+    void setXY(float x, float y)
+    {
+        m_pos.x = x;
+        m_pos.y = y;
+    }
+    void setX(float x) { m_pos.x = x; }
+    void setY(float y) { m_pos.y = y; }
+
     // Utility
     void move(float dx, float dy);
     void render() const;
+
 private:
-    SDL_Texture* imageToTexture(std::string imagePath);
+    SDL_Texture *imageToTexture(std::string imagePath);
 };
